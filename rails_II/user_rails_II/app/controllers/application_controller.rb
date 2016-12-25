e0b7c28fe02bb85c52be61e449bcdbc648031b04
Harmann Bajwa <harmannbajwa@Harmanns-MacBook-Pro.local>
2016-12-24 23:55:11 -0700

    Removed folder from repository

--- rails_II/user_rails_II/app/controllers/application_controller.rb
+++
@@ -1,7 +0,0 @@
-class ApplicationController < ActionController::Base
-  def current_user
-    User.find(session[:user_id]) if session[:user_id]
-  end
-  helper_method :current_user
-  protect_from_forgery with: :exception
-end
